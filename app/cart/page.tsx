b35eeddcbaf0eaa7db6fff157d081e97dabf0437
TEJAS204648B <chaudharitejas803@gmail.com>
2026-05-01 02:13:31 +0530

    fix cart page module error

--- app/cart/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function CartPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Cart Coming Soon 🛒</h1>
-    </div>
-  );
-}
